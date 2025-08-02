a91d8ee4332c15121db77bbd3835752af375a7c7
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-08-02 21:47:21 +0200

    begin tabs for each project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { formatDate } from "./utils.js";
 
 let projectHeader = document.querySelector('[data-id="project-title"]')
+let projectGroup = document.querySelector('[data-id="project-group"]')
 let todoDone = document.querySelector('[data-id="done"]')
 let todoTitle = document.querySelector('[data-id="title"]')
 let todoContent = document.querySelector('[data-id="description"]')
@@ -9,6 +10,20 @@ let todoCreated = document.querySelector('[data-id="created"]')
 let todoPriority = document.querySelector('[data-id="priority"]')
 
 const todos = [];
+
+function openProject(evt, input) {
+  if (input === "new") {
+    console.log("show create", input, "project!")
+  } else {
+    console.log("list project tab..")
+  }
+}
+
+document.querySelectorAll('.tablinks').forEach(btn => {
+  btn.addEventListener('click', function(event) {
+    openProject(event, btn.dataset.project);
+  })
+})
 
 function createTodo(
   project = "nytt projekt",
@@ -41,7 +56,7 @@ const newTodo1 = createTodo(
 );
 const newTodo2 = createTodo(
   "Spelutveckling",
-  true,
+  false,
   "Hav som böljar",
   "Lägg till hav som bööljar när det blåser",
   "2025-07-30",
@@ -49,7 +64,23 @@ const newTodo2 = createTodo(
 );
 const newTodo3 = createTodo(
   "Spelutveckling",
-  true,
+  false,
+  "Vind fysik",
+  "Lägg till vind fysik som får båten att segla iväg",
+  "2025-07-30",
+  "genast"
+);
+const newTodo4 = createTodo(
+  undefined,
+  false,
+  "Vind fysik",
+  "Lägg till vind fysik som får båten att segla iväg",
+  "2025-07-30",
+  "genast"
+);
+const newTodo5 = createTodo(
+  "Webutveckling",
+  false,
   "Vind fysik",
   "Lägg till vind fysik som får båten att segla iväg",
   "2025-07-30",
@@ -64,3 +95,9 @@ todoContent.textContent = todos[1].content.description
 todoCreated.textContent = todos[1].content.created
 todoDueDate.textContent = todos[1].content.dueDate
 todoPriority.textContent = todos[1].content.priority
+
+const sortedTodos = todos.slice().sort((a, b) => {
+  if (a.project < b.project) return -1;
+  if (a.project > b.project) return 1;
+  return 0;
+})
