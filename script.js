718ba06eb850ec71b388ad21cedb73ec93f559f9
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-08-02 22:45:23 +0200

    add dynamic population of tab list from projects

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 import { formatDate } from "./utils.js";
 
-let projectHeader = document.querySelector('[data-id="project-title"]')
-let projectGroup = document.querySelector('[data-id="project-group"]')
-let todoDone = document.querySelector('[data-id="done"]')
-let todoTitle = document.querySelector('[data-id="title"]')
-let todoContent = document.querySelector('[data-id="description"]')
-let todoDueDate = document.querySelector('[data-id="dueDate"]')
-let todoCreated = document.querySelector('[data-id="created"]')
-let todoPriority = document.querySelector('[data-id="priority"]')
+let tabList = document.querySelector(".tab-list");
+let projectHeader = document.querySelector('[data-id="project-title"]');
+let projectGroup = document.querySelector('[data-id="project-group"]');
+let todoDone = document.querySelector('[data-id="done"]');
+let todoTitle = document.querySelector('[data-id="title"]');
+let todoContent = document.querySelector('[data-id="description"]');
+let todoDueDate = document.querySelector('[data-id="dueDate"]');
+let todoCreated = document.querySelector('[data-id="created"]');
+let todoPriority = document.querySelector('[data-id="priority"]');
 
 const todos = [];
 
 function openProject(evt, input) {
   if (input === "new") {
-    console.log("show create", input, "project!")
+    console.log("show create", input, "project!");
   } else {
-    console.log("list project tab..")
+    console.log(
+      "list project:",
+      input,
+      todos.filter((filterProject) => input === filterProject.project)
+    );
   }
 }
-
-document.querySelectorAll('.tablinks').forEach(btn => {
-  btn.addEventListener('click', function(event) {
-    openProject(event, btn.dataset.project);
-  })
-})
 
 function createTodo(
   project = "nytt projekt",
@@ -87,23 +86,43 @@ const newTodo5 = createTodo(
   "genast"
 );
 
-projectHeader.textContent = todos[1].project
+projectHeader.textContent = todos[1].project;
 
-todoDone.checked = todos[1].content.done
-todoTitle.textContent = todos[1].content.title
-todoContent.textContent = todos[1].content.description
-todoCreated.textContent = todos[1].content.created
-todoDueDate.textContent = todos[1].content.dueDate
-todoPriority.textContent = todos[1].content.priority
+todoDone.checked = todos[1].content.done;
+todoTitle.textContent = todos[1].content.title;
+todoContent.textContent = todos[1].content.description;
+todoCreated.textContent = todos[1].content.created;
+todoDueDate.textContent = todos[1].content.dueDate;
+todoPriority.textContent = todos[1].content.priority;
 
 const sortedTodos = todos.slice().sort((a, b) => {
   if (a.project < b.project) return -1;
   if (a.project > b.project) return 1;
   return 0;
-})
+});
 
-projectGroup.innerHTML = sortedTodos.map(todo => {
-return `
+const distinctProjects = [
+  ...new Set(sortedTodos.map((project) => project.project)),
+];
+
+tabList.innerHTML += distinctProjects
+  .map((project) => {
+    return `
+    <button class="tablinks" data-project="${project}">
+          ${project}
+        </button>`;
+  })
+  .join("");
+
+document.querySelectorAll(".tablinks").forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    openProject(event, btn.dataset.project);
+  });
+});
+
+projectGroup.innerHTML = sortedTodos
+  .map((todo) => {
+    return `
   <h2 data-id="project-title">${todo.project}!</h2>
         <section class="todo-container">
           <h3 data-id="title">${todo.content.title}</h3>
@@ -120,7 +139,9 @@ return `
             </thead>
             <tbody>
               <tr>
-                <th><input type="checkbox" ${todo.content.done ? "checked" : ""} name="" data-id="done" /></th>
+                <th><input type="checkbox" ${
+                  todo.content.done ? "checked" : ""
+                } name="" data-id="done" /></th>
                 <td data-id="priority">${todo.content.priority}</td>
                 <td data-id="created">${todo.content.created}</td>
                 <td data-id="dueDate">${todo.content.dueDate}</td>
@@ -128,4 +149,6 @@ return `
             </tbody>
           </table>
         </section>
-  `}).join("");
+  `;
+  })
+  .join("");
